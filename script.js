c7f16f53586788d0108b19f5a28c086f8707fd04
topotpot <trpochynkov@gmail.com>
2024-11-26 19:56:52 +0200

    3D animation

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ function getCookie(name) {
 function setCookie(name, value) {
     document.cookie = `${name}=${JSON.stringify(value)};path=/;max-age=${3600 * 24 * 30}`;
 }
+let block = document.querySelector(".container")
+document.addEventListener("mousemove", function (e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerWidth / 2
+    let angleX = 35 * dx / window.innerWidth / 2
+    let angleY = 35 * dy / window.innerWidth / 2
+    block.style.transform = `rotateX(${angleX}deg)rotateY(${angleY}deg)`
+})
